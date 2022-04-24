01ac832558b68c675317fe3681117a72ba1164cd
minozzzi <minozzi60@gmail.com>
2022-04-23 21:19:41 -0300

    refactor(phone-mask): change function name to camelCase pattern

--- src/lib/phone-mask/index.ts
+++ src/lib/phone-mask/index.ts
@@ -1,4 +1,4 @@
 import { IPhoneMask } from './types'
 
-export const PhoneMask: IPhoneMask = input =>
+export const phoneMask: IPhoneMask = input =>
   input.replace(/\D/g, '').replace(/(\d{2})(\d{1})(\d{4})(\d)/, '$1 $2 $3-$4')
