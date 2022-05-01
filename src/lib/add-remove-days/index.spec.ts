b3e383fdd8e8f274abfd54ad1f6b0e32af5b48cd
codezeron <67158176+codezeron@users.noreply.github.com>
2022-05-01 19:06:07 -0300

    fix: changed the way date returns in UTC full date

--- src/lib/add-remove-days/index.spec.ts
+++ src/lib/add-remove-days/index.spec.ts
@@ -10,26 +10,26 @@ const date6 = new Date('2022-02-28')
 
 describe('formatToBrazilianDate', () => {
   it('should return added brazilian date ', () => {
-    expect(addDays(5, date)).toBe('2020-3-13')
+    expect(addDays(5, date)).toBe('Fri, 13 Mar 2020 00:00:00 GMT')
   })
 
   it('should return subscracted brazilian date with string param', () => {
-    expect(removeDays(5, date2)).toBe('2020-3-3')
+    expect(removeDays(5, date2)).toBe('Tue, 03 Mar 2020 00:00:00 GMT')
   })
 
   it('should return subscracted brazilian date with string param', () => {
-    expect(removeDays(5, date3)).toBe('2019-12-27')
+    expect(removeDays(5, date3)).toBe('Fri, 27 Dec 2019 00:00:00 GMT')
+  })
+
+  it('should return added brazilian date with string param', () => {
+    expect(addDays(15, date4)).toBe('Wed, 02 Mar 2022 00:00:00 GMT')
   })
 
   it('should return subscracted brazilian date with string param', () => {
-    expect(addDays(15, date4)).toBe('2022-3-2')
+    expect(removeDays(15, date5)).toBe('Mon, 31 Jan 2022 00:00:00 GMT')
   })
 
-  it('should return subscracted brazilian date with string param', () => {
-    expect(removeDays(15, date5)).toBe('2022-1-31')
-  })
-
-  it('should return subscracted brazilian date with string param', () => {
-    expect(addDays(1, date6)).toBe('2022-3-1')
+  it('should return added brazilian date with string param', () => {
+    expect(addDays(1, date6)).toBe('Tue, 01 Mar 2022 00:00:00 GMT')
   })
 })
--- src/lib/add-remove-days/index.ts
+++ src/lib/add-remove-days/index.ts
@@ -1,19 +1,11 @@
 import { IAddDays, IRemoveDays } from './types'
 
 export const addDays: IAddDays = (numberofDays, date) => {
-  date.setDate(date.getDate() + numberofDays) //o 1 serve para mostrar a data em que parou corretamente
-  const dias = date.getDate()
-  const mes = date.getMonth() + 1
-  const ano = date.getFullYear()
-  const resposta = ano + '-' + mes + '-' + dias
-  return resposta
+  date.setDate(date.getDate() + numberofDays)
+  return date.toUTCString()
 }
 
 export const removeDays: IRemoveDays = (numberofDays, date) => {
   date.setDate(date.getDate() - numberofDays)
-  const dias = date.getDate()
-  const mes = date.getMonth() + 1
-  const ano = date.getFullYear()
-  const resposta = ano + '-' + mes + '-' + dias
-  return resposta
+  return date.toUTCString()
 }
