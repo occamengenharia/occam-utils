f38d317e71d5431f12e862a653ec0f9068039d18
macerno@hotmail.com <macerno@hotmail.com>
2022-05-08 18:52:23 -0300

    feat: new function to format date to db pattern

---
+++ src/lib/format-date-to-db/index.ts
@@ -0,0 +1,27 @@
+import { DateInvalid } from 'src/shared/errors/date-invalid'
+import { IFormatDateToDB } from './types'
+
+const INVALID_DATE = 'Invalid Date'
+
+export const formatToDateDB: IFormatDateToDB = ({ date }) => {
+  const dateObject = new Date(date)
+
+  if (dateObject.toString() === INVALID_DATE)
+    throw new DateInvalid(INVALID_DATE)
+
+  const ano = dateObject.getFullYear()
+  const mes = dateObject.getMonth() + 1
+  const dia = dateObject.getDate()
+  const hora = dateObject.getHours()
+  const min = dateObject.getMinutes()
+  const seg = dateObject.getSeconds()
+  const result =
+    ano + '-' + mes + '-' + dia + ' ' + hora + ':' + min + ':' + seg
+  return result
+  // return new Intl.DateTimeFormat('pt-BR', {
+  //   year: 'numeric',
+  //   month: '2-digit',
+  //   day: '2-digit',
+  //   timeZone: 'UTC'
+  // }).format(dateObject)
+}
